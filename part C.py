3e226964ad61484217794fbac5a62b9cf7284186
B00162501 Mariam Osomoku <B00162501@mytudublin.ie>
2025-02-10 18:06:02 +0000

    Completed Part C

---
+++ part C.py
@@ -0,0 +1,17 @@
+#Mariam Osomoku
+#10 February 2025
+#A Program to Print Steps 1 to 9 to the Screen
+
+def PrintSteps(a, b):
+    print("Step 4")
+    print("Step 5")
+    print("Step 6")
+
+
+print("Step 1")
+print("Step 2")
+print("Step 3")
+PrintSteps(4,6)
+print("Step 7")
+print("Step 8")
+print("Step 9")
