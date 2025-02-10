8927df582203d5ff7cd5227a6ecc522b3f8dc319
B00162501 Mariam Osomoku <B00162501@mytudublin.ie>
2025-02-10 17:52:24 +0000

    Completed Part B

---
+++ part B.py
@@ -0,0 +1,19 @@
+#Mariam Osomoku
+#10 February 2025
+#A Program to Print Steps 1 to 9 to the Screen
+
+def PrintSteps(a,b):
+   for i in range (a, b + 1):
+    print(f"step{i}")
+
+
+
+print("Step 1")
+print("Step 2")
+print("Step 3")
+PrintSteps(4,6)
+print("Step 7")
+print("Step 8")
+print("Step 9")
+
+
